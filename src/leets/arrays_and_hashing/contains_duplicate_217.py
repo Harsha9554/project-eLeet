e1b69b9a8fa8e3db6a01d75bfcc8df520baf6901
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2023-08-27 18:14:28 -0400

    217-done

--- src/leets/arrays_and_hashing/contains_duplicate_217.py
+++ src/leets/arrays_and_hashing/contains_duplicate_217.py
@@ -20,4 +20,28 @@ def contains_duplicate_217_ii(nums):
         if a[i] == a[i+1]:
             return True
     return False
-    
+
+# hashmap, comparing seen ones
+def contains_duplicate_217_iii(nums):
+    seen = {}
+    for num in nums:
+        if num in seen and seen[num] >= 1:
+        # seen[num] >= 1?
+        # gre eq 1 ah? how?
+        #
+        # lets say arr = [9, 10, 9, 7]
+        # lets say seen = { 9: 1, 10: 1 } ila undhi.
+        # 
+        # now, after two iterations
+        # num = 9.
+        #
+        # num in seen? 9 in seen? ya.
+        # i hold 2nd 9 in my hand. 
+        # seen[num] >= 1? ya it's 1. so ya
+        # 
+        # 1 unna duplicates unnatte endhukante. i hold one number, seen already has 1 so 2 numbers are there.
+        # therefore, duplicates.
+        #
+            return True
+        seen[num] = seen.get(num, 0) + 1
+    return False
---
+++ src/leets/xhelper/hashmap-usage.py
@@ -0,0 +1,22 @@
+frequency_map = {}
+a = ["eren", "zeke", "toji", "zeke", "thorfinn", "kenzo", "dr-tenma"]
+
+
+# method #1
+# m[x] = get m[x] + 1
+# but what if there is no key x?
+# then default value = 0. by m.get(x, 0)
+
+for character in a:
+    frequency_map[character] = frequency_map.get(character, 0) + 1
+print(frequency_map)
+
+
+# method #2
+
+for character in a:
+    if character not in frequency_map:
+        frequency_map[character] = 1
+    else: frequency_map[character] += 1
+print(frequency_map)
+
